ef81b88aec789975406d9ec5a243ff7f41305f35
Leopan0 <33919622+Leopan0@users.noreply.github.com>
2023-05-16 17:37:37 +0800

    首页调整

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,14 +6,19 @@ export default defineUserConfig({
 
   locales: {
     "/": {
+      lang: "zh-CN",
+      title: "会跳舞的石头",
+      description: "代码在指尖飞舞",
+    },
+    "/en/": {
       lang: "en-US",
       title: "Blog Demo",
       description: "A blog demo for vuepress-theme-hope",
     },
-    "/zh/": {
+    "/zh": {
       lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
+      title: "会跳舞的石头",
+      description: "代码在指尖飞舞",
     },
   },
 
--- src/zh/README.md
+++ src/zh/README.md
@@ -4,9 +4,9 @@ layout: BlogHome
 icon: home
 title: 博客主页
 heroImage: /logo.svg
-heroText: 你博客的名称
+heroText: 会跳舞的石头的博客
 heroFullScreen: true
-tagline: 你可以在这里放置你的口号与标语
+tagline: 专注于指尖
 projects:
   - icon: project
     name: 项目名称
